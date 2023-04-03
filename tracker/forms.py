27177686c9216fdd1fc07090168213d8e3c521d3
Sergii Machulin <machulinsv@gmail.com>
2023-04-03 15:00:42 +0300

    Create class-based views with form for create update and delete task

---
+++ tracker/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+
+from tracker.models import Task, Tag
+
+
+class TaskForm(forms.ModelForm):
+    tags = forms.ModelMultipleChoiceField(
+        queryset=Tag.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Task
+        fields = "__all__"
