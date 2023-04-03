e245a3c0a4f545ca6d050657781d1a603e4b99ca
Sergii Machulin <machulinsv@gmail.com>
2023-04-03 11:13:51 +0300

    Add new empty app user & createsuperuser for managing admin panel

--- todo/settings.py
+++ todo/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "tracker",
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -101,6 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "user.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
