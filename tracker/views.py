eca9d247d5257604e657c74d78212c79b08fe570
Sergii Machulin <machulinsv@gmail.com>
2023-04-03 13:12:59 +0300

    Create Tag & Task views and template lists

--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from tracker.models import Tag, Task
+
+
+class TagListView(generic.ListView):
+    model = Tag
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    queryset = Task.objects.prefetch_related("tags")
